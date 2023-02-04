f8fd7bec24c43c40cf30871db59bba748f1b39a9
Mads FS <madsfs@bolverkgames.com>
2023-02-04 15:11:17 +0100

    Fixed a bug where currentViewId == 0 was not handled for GoBack

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -102,6 +102,7 @@ public class GameManager : MonoBehaviour
 	{
 		if (_currentViewId <= 0) return;
 		int previousViewId = _viewList.First(x => x.ViewId == _currentViewId).PreviousViewId;
+		if (previousViewId == 0) return;
 		View previousView = _viewList.First(x => x.ViewId == previousViewId);
 		RenderView(previousView);
 	}
